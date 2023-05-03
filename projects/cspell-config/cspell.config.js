22bc1ee5596c288483d6e74cb2759c1e89172293
A77AY <11846445+A77AY@users.noreply.github.com>
2023-05-03 12:03:12 +0400

    init configs

---
+++ projects/cspell-config/cspell.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    $schema: 'https://raw.githubusercontent.com/streetsidesoftware/cspell/main/cspell.schema.json',
+    version: '0.2',
+    ignorePaths: ['node_modules'],
+    useGitignore: true,
+    language: 'en,ru',
+    words: [
+        'devkit',
+        'Keycloak',
+        'loglevel',
+        'matero',
+        'npmrc',
+        'photoviewer',
+        'vality',
+        'valitydev',
+        'vhosts',
+    ],
+};
