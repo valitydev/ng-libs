508eddfa552aac498ff4568496285052f882f322
A77AY <11846445+A77AY@users.noreply.github.com>
2023-05-16 19:37:00 +0400

    add date fn

---
+++ projects/ng-core/src/lib/utils/date/index.ts
@@ -0,0 +1,1 @@
+export * from './get-clean-iso-string-dates';
--- projects/ng-core/src/lib/utils/index.ts
+++ projects/ng-core/src/lib/utils/index.ts
@@ -2,6 +2,7 @@ export * from './json';
 export * from './string';
 export * from './operators';
 export * from './component';
+export * from './date';
 
 export * from './clean';
 export * from './is-empty';
