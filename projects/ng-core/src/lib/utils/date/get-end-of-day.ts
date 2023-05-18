4e2972b0ef22182080340b511dbd1940aa364cfd
A77AY <11846445+A77AY@users.noreply.github.com>
2023-05-18 19:03:58 +0400

    rem end of day fn

--- projects/ng-core/src/lib/utils/date/get-end-of-day.ts
+++
@@ -1,6 +0,0 @@
-export function getEndOfDay<T extends Date | null>(date?: T): T extends Date ? Date : null {
-    if (!date) return null as never;
-    const endDate = new Date(date);
-    endDate.setUTCHours(23, 59, 59, 999);
-    return endDate as never;
-}
