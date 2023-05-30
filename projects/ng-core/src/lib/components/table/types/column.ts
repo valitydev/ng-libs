ccb2ea93ca3577490c03d93ee2fdc04065d5af82
A77AY <11846445+A77AY@users.noreply.github.com>
2023-05-31 00:09:38 +0400

    ref

--- projects/ng-core/src/lib/components/table/types/column.ts
+++ projects/ng-core/src/lib/components/table/types/column.ts
@@ -4,41 +4,44 @@ import { MtxGridColumn } from '@ng-matero/extensions/grid';
 import { SelectFn } from '../../../utils';
 
 type FormatterFn<T extends object> = SelectFn<T, unknown, [colDef: ExtColumn<T>]>;
+type TypedColumn<T = never, P = never> = {
+    type: T;
+} & Pick<
+    {
+        typeParameters: P;
+    },
+    P extends never ? never : 'typeParameters'
+>;
 
-export type BaseExtColumn<T extends object, TType = never, TTypeParams = never> = Pick<
+export type ExtColumn<T extends object> = Pick<
     MtxGridColumn<T>,
     'field' | 'header' | 'cellTemplate'
 > & {
     formatter?: FormatterFn<T>;
     description?: FormatterFn<T>;
     tooltip?: FormatterFn<T>;
-    type: TType;
-    typeParameters: TTypeParams;
-};
-
-export type ExtColumn<T extends object> =
-    | BaseExtColumn<T>
-    | BaseExtColumn<T, 'datetime'>
-    | BaseExtColumn<T, 'currency', { currencyCode: FormatterFn<T> }>
-    | BaseExtColumn<
-          T,
-          'tag',
-          {
-              tags: Record<
-                  PropertyKey,
-                  { label?: string; color?: 'success' | 'pending' | ThemePalette }
-              >;
-          }
-      >
-    | BaseExtColumn<
-          T,
-          'menu',
-          {
-              items: {
-                  label: string;
-                  click: (rowData: T) => void;
-              }[];
-          }
-      >;
+} & (
+        | { type?: undefined }
+        | TypedColumn<'datetime'>
+        | TypedColumn<'currency', { currencyCode: FormatterFn<T> }>
+        | TypedColumn<
+              'tag',
+              {
+                  tags: Record<
+                      PropertyKey,
+                      { label?: string; color?: 'success' | 'pending' | ThemePalette }
+                  >;
+              }
+          >
+        | TypedColumn<
+              'menu',
+              {
+                  items: {
+                      label: string;
+                      click: (rowData: T) => void;
+                  }[];
+              }
+          >
+    );
 
 export type Column<T extends object> = ExtColumn<T> | string;
