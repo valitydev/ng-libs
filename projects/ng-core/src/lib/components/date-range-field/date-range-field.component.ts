5b97f3656c722a27709480f2672fc4733b210024
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-09 14:44:13 +0400

    fix

--- projects/ng-core/src/lib/components/date-range-field/date-range-field.component.ts
+++ projects/ng-core/src/lib/components/date-range-field/date-range-field.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
+import { DateAdapter, MAT_DATE_LOCALE, NativeDateAdapter } from '@angular/material/core';
 import { coerceBoolean } from 'coerce-property';
 
 import { FormGroupSuperclass, createControlProviders } from '../../utils';
@@ -13,7 +14,10 @@ export interface DateRange {
     selector: 'v-date-range-field',
     templateUrl: './date-range-field.component.html',
     styleUrls: ['./date-range-field.component.scss'],
-    providers: createControlProviders(() => DateRangeFieldComponent),
+    providers: [
+        ...createControlProviders(() => DateRangeFieldComponent),
+        { provide: DateAdapter, useClass: NativeDateAdapter, deps: [MAT_DATE_LOCALE] },
+    ],
 })
 export class DateRangeFieldComponent extends FormGroupSuperclass<DateRange> {
     @Input() @coerceBoolean required: boolean | '' = false;
