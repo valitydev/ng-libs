44aaa6d1055acbb07f9452cd7f0f2af3a6c1f01f
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-28 17:54:26 +0400

    ref

--- projects/ng-core/src/lib/utils/form/form-wrappers/abstract-control-superclass.ts
+++ projects/ng-core/src/lib/utils/form/form-wrappers/abstract-control-superclass.ts
@@ -4,22 +4,12 @@ import isEqual from 'lodash-es/isEqual';
 import { EMPTY, Observable, switchMap } from 'rxjs';
 import { distinctUntilChanged, map, startWith } from 'rxjs/operators';
 
-import { hasControls } from '../has-controls';
-
 import { getErrorsTree } from './utils/get-errors-tree';
 
 export abstract class AbstractControlSuperclass<
     OuterType,
     InnerType = OuterType
 > extends WrappedControlSuperclass<OuterType, InnerType> {
-    override setDisabledState(isDisabled: boolean): void {
-        // If the form group control is disabled, then it turns it on during initialization
-        if (hasControls(this.control)) {
-            return;
-        }
-        super.setDisabledState(isDisabled);
-    }
-
     protected override setUpInnerToOuterErrors$(
         inner$: Observable<ValidationErrors>
     ): Observable<ValidationErrors> {
