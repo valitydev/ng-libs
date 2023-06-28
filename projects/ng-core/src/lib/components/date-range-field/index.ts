d621910db2fca44fa4d3cc3d8ccc7662041c9a8a
A77AY <11846445+A77AY@users.noreply.github.com>
2023-06-29 00:21:24 +0400

    add daterange utils

--- projects/ng-core/src/lib/components/date-range-field/index.ts
+++ projects/ng-core/src/lib/components/date-range-field/index.ts
@@ -1,3 +1,5 @@
 export * from './date-range-field.module';
 export * from './date-range-field.component';
 export * from './types/date-range';
+export * from './utils/is-equal-date-range';
+export * from './utils/create-date-range-to-today';
