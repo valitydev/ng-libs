2bac5cee51c69610e39ae87ea96577b58637309e
A77AY <11846445+A77AY@users.noreply.github.com>
2023-07-04 20:50:13 +0400

    supported exts

--- projects/ng-core/src/lib/components/file-upload/file-upload.component.ts
+++ projects/ng-core/src/lib/components/file-upload/file-upload.component.ts
@@ -1,5 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { NotifyLogService } from '../../services';
+
+function hasExtension(fileName: string, extensions: string[]) {
+    return (
+        !extensions?.length ||
+        new RegExp('(' + extensions.join('|').replace(/\./g, '\\.') + ')$').test(fileName)
+    );
+}
+
 @Component({
     selector: 'v-file-upload',
     templateUrl: './file-upload.component.html',
@@ -7,13 +16,25 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class FileUploadComponent {
     @Input() label?: string;
+    @Input() extensions: string[] = [];
 
     @Output() upload = new EventEmitter<File | null>();
 
     protected file?: File | null;
 
+    constructor(private log: NotifyLogService) {}
+
     loadFile(event: Event) {
-        this.file = (event.target as HTMLInputElement)?.files?.[0] ?? null;
+        const file = (event.target as HTMLInputElement)?.files?.[0] ?? null;
+        if (!file) {
+            this.log.error(new Error('File upload error'));
+            return;
+        }
+        if (!hasExtension(file.name, this.extensions)) {
+            this.log.error(new Error(`Supported file types: ${this.extensions.join(', ')}`));
+            return;
+        }
+        this.file = file;
         this.upload.emit(this.file);
     }
 }
