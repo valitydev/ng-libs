84da2ebee44ca1eea201cf4f72ce2ae3b2c0b154
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-10-18 02:17:28 +0700

    upd input

--- projects/ng-core/src/lib/components/input-field/input-field.component.ts
+++ projects/ng-core/src/lib/components/input-field/input-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MatFormFieldAppearance } from '@angular/material/form-field';
 
 import { createControlProviders, FormControlSuperclass } from '../../utils';
 
@@ -10,4 +11,5 @@ import { createControlProviders, FormControlSuperclass } from '../../utils';
 export class InputFieldComponent<T> extends FormControlSuperclass<T> {
     @Input() label?: string;
     @Input() type: 'string' | 'number' = 'string';
+    @Input() appearance: MatFormFieldAppearance = 'fill';
 }
