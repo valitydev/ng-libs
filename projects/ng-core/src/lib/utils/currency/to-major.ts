526003f1dc5d4a0fa3d678802715df6039df7112
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-02-15 22:34:59 +0700

    add toMinorByExponent

--- projects/ng-core/src/lib/utils/currency/to-major.ts
+++ projects/ng-core/src/lib/utils/currency/to-major.ts
@@ -2,8 +2,8 @@ import round from 'lodash-es/round';
 
 import { getCurrencyExponent } from './get-currency-exponent';
 
-export const toMajor = (amount: number, currencyCode: string): number =>
-    toMajorByExponent(amount, getCurrencyExponent(currencyCode));
-
 export const toMajorByExponent = (amount: number, exponent: number): number =>
     round(amount / 10 ** exponent, exponent);
+
+export const toMajor = (amount: number, currencyCode: string): number =>
+    toMajorByExponent(amount, getCurrencyExponent(currencyCode));
--- projects/ng-core/src/lib/utils/currency/to-minor.ts
+++ projects/ng-core/src/lib/utils/currency/to-minor.ts
@@ -1,4 +1,7 @@
 import { getCurrencyExponent } from './get-currency-exponent';
 
+export const toMinorByExponent = (amount: number, exponent: number): number =>
+    Math.round(amount * 10 ** exponent);
+
 export const toMinor = (amount: number, currencyCode: string): number =>
-    Math.round(amount * 10 ** getCurrencyExponent(currencyCode));
+    toMinorByExponent(amount, getCurrencyExponent(currencyCode));
