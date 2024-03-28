d9bdee44fc94920341df9db741efba35df1182a7
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-03-29 05:44:23 +0900

    add links

--- projects/ng-libs-doc/src/app/app.component.ts
+++ projects/ng-libs-doc/src/app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from '@ng-doc/app';
+import {
+    NgDocRootComponent,
+    NgDocNavbarComponent,
+    NgDocSidebarComponent,
+    NgDocThemeToggleComponent,
+} from '@ng-doc/app';
+import {
+    NgDocButtonIconComponent,
+    NgDocTooltipDirective,
+    NgDocIconComponent,
+} from '@ng-doc/ui-kit';
 
 @Component({
     selector: 'app-root',
@@ -11,8 +21,14 @@ import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from 
         NgDocNavbarComponent,
         NgDocSidebarComponent,
         RouterLink,
+        NgDocThemeToggleComponent,
+        NgDocButtonIconComponent,
+        NgDocTooltipDirective,
+        NgDocIconComponent,
     ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+    githubUrl = 'https://github.com/valitydev/ng-libs';
+}
