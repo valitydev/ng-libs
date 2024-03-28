a4676aea3d86b5c162ee2835e43b7b000f8bcf07
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-03-28 23:20:32 +0900

    add ng-doc

--- projects/ng-libs-doc/src/app/app.component.ts
+++ projects/ng-libs-doc/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from "@ng-doc/app";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
