a31a24414c52636646b771fceb7f3a112c1c5c5d
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-04-25 01:35:20 +0900

    add handle-error

--- projects/ng-core/src/lib/utils/operators/index.ts
+++ projects/ng-core/src/lib/utils/operators/index.ts
@@ -4,3 +4,4 @@ export * from './pass-error';
 export * from './fork-join-to-result';
 export * from './switch-combine-with';
 export * from './debounce-time-with-first';
+export * from './handle-error';
