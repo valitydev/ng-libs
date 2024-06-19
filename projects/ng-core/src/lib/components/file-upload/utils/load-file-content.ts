0a6dad2bdcddc101cd1f3c2475c527d2b543cf28
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-06-19 12:59:44 +0500

    add param

--- projects/ng-core/src/lib/components/file-upload/utils/load-file-content.ts
+++ projects/ng-core/src/lib/components/file-upload/utils/load-file-content.ts
@@ -1,12 +1,12 @@
 import { Observable } from 'rxjs';
 
-export function loadFileContent(file: File): Observable<string> {
+export function loadFileContent(file: File, encoding = 'utf-8'): Observable<string> {
     return new Observable((subscriber) => {
         if (!file) {
             return file;
         }
         const read = new FileReader();
-        read.readAsText(file, 'utf-8');
+        read.readAsText(file, encoding);
         read.onloadend = function () {
             if (read.error) {
                 return subscriber.error(read.error);
