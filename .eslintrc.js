e72dd8e0fbb76d78191ba88a0b633ed4e030524b
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-06-21 02:57:37 +0500

    add resolve-plugins-relative-to

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,10 @@ const NG_CORE_PATH = 'projects/ng-core/**';
 const NG_LIBS_DOC_PATH = 'projects/ng-libs-doc/**';
 
 module.exports = {
-    ...config,
-    ignorePatterns: [
-        ...config.ignorePatterns,
-        'ng-doc'
-    ],
+    extends: '@vality/eslint-config-ng',
+    ignorePatterns: ['ng-doc'],
     overrides: [
-        ...config.overrides,
-        ...require('./projects/eslint-config/configs/angular')('v', NG_CORE_PATH).overrides,
-        ...require('./projects/eslint-config/configs/angular')('app', NG_LIBS_DOC_PATH).overrides,
+        ...require('@vality/eslint-config-ng/configs').angular('v', NG_CORE_PATH).overrides,
+        ...require('@vality/eslint-config-ng/configs').angular('app', NG_LIBS_DOC_PATH).overrides,
     ],
 };
