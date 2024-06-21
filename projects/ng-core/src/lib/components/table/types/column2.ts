b8caed5bbe367fabfd4e567563f86b3f72b95c05
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-06-22 00:38:18 +0500

    fix header cell type

--- projects/ng-core/src/lib/components/table/types/column2.ts
+++ projects/ng-core/src/lib/components/table/types/column2.ts
@@ -1,6 +1,6 @@
 import { get } from 'lodash-es';
 import startCase from 'lodash-es/startCase';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
 import { PossiblyAsync, getPossiblyAsyncObservable } from '../../../utils';
@@ -8,14 +8,13 @@ import { Value } from '../../value';
 
 type Fn<R, P extends Array<unknown> = []> = (...args: P) => R;
 type PossiblyFn<R, P extends Array<unknown> = []> = Fn<R, P> | R;
-type ColumnValue = Value | string;
 
 type CellFnArgs<T extends object> = [data: T, index: number];
 
 export interface Column2<T extends object> {
     field: string;
-    header?: PossiblyAsync<ColumnValue>;
-    cell?: PossiblyFn<PossiblyAsync<ColumnValue>, CellFnArgs<T>>;
+    header?: PossiblyAsync<Partial<Value> | string>;
+    cell?: PossiblyFn<PossiblyAsync<Partial<Value> | string>, CellFnArgs<T>>;
 }
 
 export interface NormalizedColumn2<T extends object> {
@@ -28,9 +27,16 @@ export function normalizePossiblyFn<R, P extends Array<unknown>>(fn: PossiblyFn<
     return typeof fn === 'function' ? (fn as Fn<R, P>) : () => fn;
 }
 
-export function normalizeColumnValue(v: PossiblyAsync<ColumnValue>): Observable<Value> {
-    return getPossiblyAsyncObservable(v).pipe(
-        map((value) => (typeof value === 'object' ? value : { value })),
+export function normalizeColumnValue(
+    sourceValue?: PossiblyAsync<Partial<Value> | string>,
+    defaultValue?: Value['value'],
+): Observable<Value> {
+    return getPossiblyAsyncObservable(sourceValue ?? defaultValue).pipe(
+        map((value) =>
+            typeof value === 'object'
+                ? ({ value: defaultValue, ...value } as Value)
+                : { value: value ?? defaultValue },
+        ),
         shareReplay({ refCount: true, bufferSize: 1 }),
     );
 }
@@ -38,10 +44,11 @@ export function normalizeColumnValue(v: PossiblyAsync<ColumnValue>): Observable<
 export function normalizeColumns<T extends object>(columns: Column2<T>[]): NormalizedColumn2<T>[] {
     return columns.map((c) => ({
         field: c.field,
-        header: normalizeColumnValue(c.header ?? startCase(c.field)),
-        cell: (v, ...args) =>
-            c.cell
-                ? normalizeColumnValue(normalizePossiblyFn(c.cell)(v, ...args))
-                : of({ value: get(v, c.field) } satisfies Value),
+        header: normalizeColumnValue(c.header, startCase(c.field)),
+        cell: (d, ...args) =>
+            normalizeColumnValue(
+                c.cell ? normalizePossiblyFn(c.cell)(d, ...args) : undefined,
+                get(d, c.field),
+            ),
     }));
 }
