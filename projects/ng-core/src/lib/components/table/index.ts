02c8710c56c7ee9e18a2fee36c1aee5571246643
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-09-19 03:41:33 +0900

    cachedHeadMap

--- projects/ng-core/src/lib/components/table/index.ts
+++ projects/ng-core/src/lib/components/table/index.ts
@@ -3,6 +3,7 @@ export * from './table.module';
 export * from './utils/create-columns-objects';
 export * from './utils/correct-priorities';
 export * from './utils/create-column';
+export * from './utils/cached-head-map';
 export * from './components/table2';
 export * from './components/table-actions.component';
 export * from './components/table-inputs.component';
