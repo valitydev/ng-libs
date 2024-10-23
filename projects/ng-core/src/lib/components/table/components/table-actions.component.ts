3d19f6d60c421e1b5a929adf92ed0fdaea1559e7
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2024-10-24 00:55:02 +0700

    rem unused

--- projects/ng-core/src/lib/components/table/components/table-actions.component.ts
+++ projects/ng-core/src/lib/components/table/components/table-actions.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActionsModule } from '../../actions';
 
 @Component({
     selector: 'v-table-actions',
+    standalone: true,
     template: `
         <v-actions>
             <ng-content></ng-content>
         </v-actions>
     `,
+    imports: [ActionsModule],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableActionsComponent {}
--- projects/ng-core/src/lib/components/table/table.module.ts
+++ projects/ng-core/src/lib/components/table/table.module.ts
@@ -1,67 +1,11 @@
-import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink } from '@angular/router';
-import { MtxTooltip } from '@ng-matero/extensions/tooltip';
 
-import { PipesModule } from '../../pipes';
-import { ActionsModule } from '../actions';
-import { InputFieldModule } from '../input-field';
-import { SwitchButtonModule } from '../switch-button';
-import { TagModule } from '../tag';
-
-import { ScoreColumnComponent } from './components/score-column.component';
-import { SelectColumnComponent } from './components/select-column.component';
 import { TableActionsComponent } from './components/table-actions.component';
 import { TableInputsComponent } from './components/table-inputs.component';
 import { TableComponent } from './table.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        MatTableModule,
-        MatCardModule,
-        MatProgressSpinnerModule,
-        MatInputModule,
-        MatSelectModule,
-        MatIconModule,
-        MatMenuModule,
-        MatButtonModule,
-        ActionsModule,
-        MatTooltipModule,
-        MatChipsModule,
-        PipesModule,
-        TagModule,
-        RouterLink,
-        MatProgressBarModule,
-        MatCheckboxModule,
-        MatSortModule,
-        InputFieldModule,
-        ReactiveFormsModule,
-        SwitchButtonModule,
-        CdkDrag,
-        CdkDropList,
-        SelectColumnComponent,
-        ScoreColumnComponent,
-        MtxTooltip,
-        TableComponent,
-        TableInputsComponent,
-    ],
-    declarations: [TableActionsComponent],
+    imports: [TableActionsComponent, TableComponent, TableInputsComponent],
     exports: [TableActionsComponent, TableInputsComponent, TableComponent],
 })
 export class TableModule {}
